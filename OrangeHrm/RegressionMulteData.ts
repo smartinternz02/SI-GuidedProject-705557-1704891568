69cea53c626ad4c189b1abf39a5b17d5d6ef9e45
Ankit kumar jha <107815079+Ankit8848@users.noreply.github.com>
2024-02-04 02:25:42 -0800

    Create RegressionMulteData.ts

---
+++ OrangeHrm/RegressionMulteData.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RegressionMulteData</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0df983ad-6c59-4556-9fa2-118140cdfd16</testSuiteGuid>
+   <testCaseLink>
+      <guid>e4522918-e485-46a7-9c5d-b04374c20eeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_OHRM_LOGIN_001</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>24ee0152-57c6-4297-83cb-48c80d02b5d9</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Excel Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>24ee0152-57c6-4297-83cb-48c80d02b5d9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>e44b3b37-31f8-4d99-83e8-e19519d863a4</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>24ee0152-57c6-4297-83cb-48c80d02b5d9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>3e521d2a-7fbf-4390-ac30-b42ed9ce9702</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
